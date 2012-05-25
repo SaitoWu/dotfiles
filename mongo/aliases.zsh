f4726e86e0ff3011d77887e825e142b4f8717cff
Saito <saitowu@gmail.com>
2012-05-26 01:01:25 +0800

    add mongodb alias

---
+++ mongo/aliases.zsh
@@ -0,0 +1,2 @@
+alias mongo-up="mongod run --logpath=/usr/local/var/log/mongodb/mongodb.log --logappend --fork --config /usr/local/Cellar/mongodb/2.0.3-x86_64/mongod.conf"
+alias mongo-down="killall mongod"
