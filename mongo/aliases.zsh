95480b7f4741481135084ebb29acda05fd24bdce
Saito <saitowu@gmail.com>
2012-09-18 18:20:12 +0800

    use shorter alias

--- mongo/aliases.zsh
+++ mongo/aliases.zsh
@@ -1,2 +1,2 @@
-alias mongo-up="mongod run --logpath=/usr/local/var/log/mongodb/mongodb.log --logappend --fork --config /usr/local/Cellar/mongodb/2.0.3-x86_64/mongod.conf"
-alias mongo-down="killall mongod"
+alias mu="mongod run --logpath=/usr/local/var/log/mongodb/mongodb.log --logappend --fork --config /usr/local/Cellar/mongodb/2.2.0-x86_64/mongod.conf"
+alias md="killall mongod"
--- postgres/aliases.zsh
+++ postgres/aliases.zsh
@@ -1,2 +1,2 @@
-alias pg-up='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
-alias pg-down='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
+alias pu='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
+alias pd='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
