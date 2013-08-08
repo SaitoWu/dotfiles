65dfe9477f1e0de2cc27718323d1c9678186fb93
Saito <saitowu@gmail.com>
2013-08-08 18:17:37 +0800

    add brew cask options

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,3 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,4 +8,3 @@ alias eh='sudo vim /etc/hosts'
 alias sj='e /usr/local/Cellar/jetty/8.1.8/libexec/contexts'
 alias cleands='find . -name .DS_Store -type f -exec rm -v {} \;'
 alias fixow='/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user;killall Finder;echo "Open With has been rebuilt, Finder will relaunch"'
-
