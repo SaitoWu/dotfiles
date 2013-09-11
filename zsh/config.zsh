1b1f0c08550315a2959a7ba816ac863be2439cf0
Saito <saitowu@gmail.com>
2013-09-11 14:23:55 +0800

    add z to path

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -43,10 +43,4 @@ setopt complete_aliases
 
 zle -N newtab
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
+bindkey -e
