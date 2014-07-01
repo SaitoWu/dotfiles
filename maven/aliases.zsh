6e4bd79959f34aec7ca495ceca625f61a25507ec
Saito <saitowu@gmail.com>
2014-07-01 12:07:25 +0800

    update some dots

--- maven/aliases.zsh
+++ maven/aliases.zsh
@@ -1,3 +1,3 @@
 alias mc="mvn clean install -Dmaven.test.skip -U"
 alias mp="mvn clean package -Dmaven.test.skip -U"
-alias mr="mvn clean package -Dmaven.test.skip -U && jetty run"
+alias mr="mvn clean package -Dmaven.test.skip -U && jetty restart"
--- vagrant/aliases.zsh
+++ vagrant/aliases.zsh
@@ -1,3 +1,4 @@
 alias vs="vagrant ssh"
 alias vu="vagrant up"
 alias vd="vagrant halt"
+alias vp="vagrant provision"
